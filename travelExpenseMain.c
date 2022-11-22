61c2c911ca23f0cb789f762288de7949965af0ff
Maung945 <myoaung@cpp.edu>
2022-11-22 14:18:21 -0800

    First Push

---
+++ travelExpenseMain.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+double milageCalc(doulbe miles);
+double parkingFeesCalc(double  parking);
+double lodgingCalc(double hotel);
+double totalDaysCount(int days);
+
+int main(void) {
+    int numberOfDays;
+    double departure, arrival;
+
+    return EXIT_SUCCESS;
+}
