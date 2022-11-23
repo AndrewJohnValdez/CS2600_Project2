fffba7d6397571a59b23aade4433bc3e0d150145
Allen <allenchiang@cpp.edu>
2022-11-22 23:12:38 -0800

    added travelFees.c and created plans for it

---
+++ travelFees.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stddef.h>
+
+double airfareAmount();
+    //ask user for amount of times of round trip airfare
+    //use a loop to ask and add to a total to be returned as the total cost of airfare
+
+double taxiAmount();
+    //ask user for amount of days to determine company allowed amount
+    //use a loop to ask and add to a total to be retuned as total cost of taxi fees
+    //subtract total cost by 10*days to determine total cost of taxi fees in regards to the company allowed amount
+
+double carRental();
+    //ask user amount of car rentals made
+    //use a loop to ask and add to a total to be returned as toatal cost of car rentals
+    
