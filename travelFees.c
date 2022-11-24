555df416ffc15817f22eeafc90f58d7dcaecaafe
Allen <allenchiang@cpp.edu>
2022-11-23 16:31:38 -0800

    added travelFees.exe and travelFeesTest.c for testing purposes. Added traveFees.h so travelFees can be used in other files. Added test print messages in travelFees.c an travelFeesTest.c

--- travelFees.c
+++ travelFees.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include "travelFees.h"
 
-double airfareAmount();
+double airfareAmount()
+{
     //ask user for amount of times of round trip airfare
     //use a loop to ask and add to a total to be returned as the total cost of airfare
-
-double taxiAmount();
+    printf("test airfare\n");
+    return 0.0;
+} 
+double taxiAmount()
+{
     //ask user for amount of days to determine company allowed amount
     //use a loop to ask and add to a total to be retuned as total cost of taxi fees
     //subtract total cost by 10*days to determine total cost of taxi fees in regards to the company allowed amount
-
-double carRental();
+    printf("test taxi\n");
+    return 0.0;
+}
+double carRental()
+{
     //ask user amount of car rentals made
     //use a loop to ask and add to a total to be returned as toatal cost of car rentals
+    printf("test car rental\n");
+    return 0.0;
+}
+    
     
